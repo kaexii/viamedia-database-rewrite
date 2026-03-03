605eba2f0504354a116f69078054114ab84cb744
Kay <true.kay.the.animator@gmail.com>
2026-03-03 18:12:55 -0500

    man i fucking hate env variables

--- api/upload.js
+++ api/upload.js
@@ -1,8 +1,8 @@
 import { Redis } from "@upstash/redis";
 
 const redis = new Redis({
-  url: "https://unbiased-cougar-63250.upstash.io",
-  token: "AfcSAAIncDJjNDcyODBkMWIxOTE0NDA3YjU4MTBmZTJiYjUzNmMxOHAyNjMyNTA",
+  url: process.env.UPSTASH_REDIS_REST_URL,
+  token: process.env.UPSTASH_REDIS_REST_TOKEN,
 });
 
 export default async function handler(req, res) {
